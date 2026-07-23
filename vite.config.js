80229c9a42d8a3caf04455dd36189ebd8163b572
Alperen Kılıç <alperenkilic@172-2-0-154.lightspeed.dybhfl.sbcglobal.net>
2026-07-23 15:36:39 +0300

    fix: disable babel-plugin-react-compiler causing vercel build failures

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["babel-plugin-react-compiler", { target: "19" }],
-        ],
-      },
-    }),
+    react(),
   ],
 })
